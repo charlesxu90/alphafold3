12aa0c09c3038907ffea6aa8bd4803b795d9f414
Xiaopeng Xu <charlesxu90@gmail.com>
2025-01-15 09:29:54 +0300

    save af3 code

--- run_af3_local.sh
+++ run_af3_local.sh
@@ -8,13 +8,26 @@ AF3_path=/home/xiaopeng/Desktop/Struct_pred/alphafold3
 # input_json=$AF3_path/output/phytase_phytic_acid/phytase_phytic_acid_data.json
 
 # Phytase L32
-input_json=$AF3_path/output/L32_phytic_acid/L32_phytic_acid_data.json
+data_path=/mnt/data/done_projects/Prot_pred/ref_works/0.struct_pred/alphafold3/results/phytase
+# input_json=$data_path/l32/data.json
+# input_json=$data_path/l32_2p/data.json
+# input_json=$data_path/l32_3p/data.json
+# input_json=$data_path/l32_4p/data.json
+# input_json=$data_path/l32_5p/data.json
+# input_json=$data_path/l32_5p_ccd/data.json
+input_json=$data_path/l32_6p/data.json
 
 XLA_FLAGS="--xla_gpu_enable_triton_gemm=false" CUDA_VISIBLE_DEVICES=0 \
 	$PY3 run_alphafold.py \
 	--json_path=$input_json \
 	--model_dir=$AF3_path/model \
 	--db_dir=$AF3_path/alphafold3_db \
-	--output_dir=$AF3_path/output \
+	--output_dir=$data_path \
 	--run_data_pipeline=false --run_inference=true
 	# --flash_attention_implementation=xla # need if GPU is not A100
+
+# Batch converting cif to pdb
+# for i in */*.cif; do maxit -input $i -output $i.pdb -o 2; done 
+
+# Batch running RING for a directory
+# for i in */*.pdb; do /home/xiaopeng/Desktop/Enzyme_RIN/ref_works/1.dccm_rin/ring-4.0/out/bin/ring -i $i --out_dir  $(dirname $i); done
